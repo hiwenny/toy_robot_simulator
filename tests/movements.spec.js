036058e7f1636cdc8a5ba37230cc783d8654b017
Wenny Hidayat <wenny.h@live.com>
2020-01-18 16:25:18 +1100

    Added some more tests

--- tests/movements.spec.js
+++ tests/movements.spec.js
@@ -18,6 +18,12 @@ describe('setPosition', () => {
   }
   it('acts on MOVE instruction by applying movement pattern in specified direction', () => {
     expect(movements.setPosition(robotPosition, 'MOVE')).toEqual({ ...robotPosition, X: robotPosition.X+1 })
+  })  
+  it('acts on LEFT instruction by changing the direction left of original orientation', () => {
+    expect(movements.setPosition(robotPosition, 'LEFT')).toEqual({ ...robotPosition, direction: constants.validDirections.NORTH})
+  })  
+  it('acts on RIGHT instruction by changing the direction right of original orientation', () => {
+    expect(movements.setPosition(robotPosition, 'RIGHT')).toEqual({ ...robotPosition, direction: constants.validDirections.SOUTH })
   })
   it('returns initial robot position if passed in an invalid command', () => {
     expect(movements.setPosition(robotPosition, 'INVALID')).toEqual(robotPosition)
